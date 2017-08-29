2b6d77084a9bf4c0996907ec1befc9b6ab82c22e
Andrew <nowosad@inbox.ru>
2017-08-29 14:37:50 +0300

    Добавлены вьюмодельные DelegateCommand и Vm

---
+++ Max8.NET/ViewModels/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Max8.NET.ViewModels
+{
+    class DelegateCommand : ICommand
+    {
+        protected readonly Predicate<object> _canExecute;
+        protected readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute) : this(execute, _ => true) { }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public void RaiseCanExecuteChanged()
+            => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
