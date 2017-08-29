aa17833b4b2760b433d81e39c1fe8dc1351865b3
Andrew <nowosad@inbox.ru>
2017-08-29 14:09:01 +0300

    Добавлены модельные Field и Cell

---
+++ Max8.NET/Models/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Max8.NET.Models
+{
+    struct Cell
+    {
+        static Random random = new Random();
+        static int GetRandomValue()
+        {
+            int v = random.Next(-Range, Range);
+            if (v >= 0) v++;
+            return v;
+        }
+
+        public static readonly int Range = 8;
+
+        public static Cell CreateRandomCell()
+            => new Cell(GetRandomValue());
+
+        public int Value { get; }
+        public bool IsActive { get; set; }
+
+        Cell(int value)
+        {
+            Value = value;
+            IsActive = true;
+        }
+    }
+}
---
+++ Max8.NET/Models/Field.cs
@@ -0,0 +1,37 @@
+﻿namespace Max8.NET.Models
+{
+    class Field
+    {
+        public static readonly int Width = 8;
+
+        public static Field CreateRandom()
+        {
+            var f = new Field();
+            f.cells = new Cell[Width, Width];
+            for (int x = 0; x < Width; ++x)
+                for (int y = 0; y < Width; ++y)
+                    f.cells[x, y] = Cell.CreateRandomCell();
+            return f;
+        }
+
+        Cell[,] cells;
+        bool isTransp = false;
+
+        Field() { }
+
+        public Cell this[int x, int y]
+            => isTransp ? cells[y, x] : cells[x, y];
+
+        public void Transpose()
+            => isTransp = !isTransp;
+
+        public Field CreateCopy()
+        {
+            return new Field
+            {
+                cells = (Cell[,])cells.Clone(),
+                isTransp = isTransp
+            };
+        }
+    }
+}
