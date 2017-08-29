7494f30ef5dd7b46684c29dd39ea4ee67fee5d95
Andrew <nowosad@inbox.ru>
2017-08-29 15:04:04 +0300

    Добавлены вьюмодельные CellVm и PlayerVm

---
+++ Max8.NET/ViewModels/CellVm.cs
@@ -0,0 +1,20 @@
+﻿namespace Max8.NET.ViewModels
+{
+    class CellVm : Vm
+    {
+        public int Value { get; }
+
+        bool isActive;
+        public bool IsActive
+        {
+            get => isActive;
+            set => Set(ref isActive, value);
+        }
+
+        public CellVm(int value)
+        {
+            Value = value;
+            IsActive = true;
+        }
+    }
+}
---
+++ Max8.NET/ViewModels/PlayerVm.cs
@@ -0,0 +1,26 @@
+﻿using Max8.NET.Models;
+
+namespace Max8.NET.ViewModels
+{
+    class PlayerVm : Vm
+    {
+        public string Name { get; }
+        public bool IsAi { get; }
+        public Max8Ai Ai { get; }
+
+        int score;
+        public int Score
+        {
+            get => score;
+            set => Set(ref score, value);
+        }
+
+        public PlayerVm(string name, bool isAi, Max8Ai ai)
+        {
+            Name = name;
+            IsAi = isAi;
+            Ai = ai;
+            Score = 0;
+        }
+    }
+}
