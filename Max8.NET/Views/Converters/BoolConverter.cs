86c791475552d2d6dc1ed9702e95f4c8f2d643ff
Andrew <nowosad@inbox.ru>
2017-08-30 13:33:20 +0300

    Реализована логика показа/скрытия кнопок

---
+++ Max8.NET/Views/Converters/BoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Max8.NET.Views.Converters
+{
+    class BoolConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b1 && b1) return TrueValue;
+            if (value is bool b2 && !b2) return FalseValue;
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, TrueValue)) return true;
+            if (Equals(value, FalseValue)) return false;
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
