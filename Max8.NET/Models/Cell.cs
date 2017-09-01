efc0362faf6d90c75f51629c40c7e9eb0c4273d8
Andrew <nowosad@inbox.ru>
2017-09-01 08:58:09 +0300

    Рефакторинг Cell

--- Max8.NET/Models/Cell.cs
+++ Max8.NET/Models/Cell.cs
@@ -4,18 +4,16 @@ namespace Max8.NET.Models
 {
     struct Cell
     {
+        public static readonly int Range = 8;
+
         static Random random = new Random();
-        static int GetRandomValue()
+
+        public static Cell CreateRandomCell()
         {
             int v = random.Next(-Range, Range);
             if (v >= 0) v++;
-            return v;
+            return new Cell(v);
         }
-
-        public static readonly int Range = 8;
-
-        public static Cell CreateRandomCell()
-            => new Cell(GetRandomValue());
 
         public int Value { get; }
         public bool IsActive { get; set; }
